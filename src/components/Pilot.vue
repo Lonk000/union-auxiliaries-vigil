709d68e763b81978bf30657656d408d18b86e716
Lonk of Hyrool <aryanmomen2005@gmail.com>
2026-02-04 15:38:24 -0700

    commented out coldcore

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -122,7 +122,7 @@ import longrimData from 'lancer-longrim-data'
 
 
 import wallflowerData from '@/assets/LCPs/wallflower-data-2.0.5'
-import coldcoreColiseumData from '@/assets/LCPs/COLDCORE_COLISEUM_V2.0' 
+// import coldcoreColiseumData from '@/assets/LCPs/COLDCORE_COLISEUM_V2.0' 
 import fieldGuideToSuldanData from '@/assets/LCPs/Field_Guide_To_Suldan_2.2.6'
 import intercorpData from '@/assets/LCPs/Intercorp 3.2'
 import dustgraveData from '@/assets/LCPs/dustgrave-data-1.4.0'
