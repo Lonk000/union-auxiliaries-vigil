8a37413e8fbd914c0458b1478b71f6c3a95deb77
Lonk of Hyrool <aryanmomen2005@gmail.com>
2026-02-04 15:52:41 -0700

    sotw index

--- src/assets/LCPs/sotw-data-1.0.2/index.js
+++ src/assets/LCPs/sotw-data-1.0.2/index.js
@@ -3,16 +3,12 @@ import b from "./bonds.json"
 import f from "./frames.json"
 import p from "./pilot_gear.json"
 import s from "./systems.json"
-import w from "./weapons.json"
-import t from "./talents.json"
 
 const data = {
   bonds: b,
   frames: f,
   pilot_gear: p,
   systems: s,
-  weapons: w,
-  talents: t,
 }
 
 export default data;
